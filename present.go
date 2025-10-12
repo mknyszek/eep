ede9c0ef17f045ab3223d19d9b03db4ee3537e52
Michael Knyszek <mknyszek@gmail.com>
2025-10-12 01:12:56 -0400

    eep: make SlideDeck interface to generalize Deck
    This should make it easier to design decks with more fluid transitions.
    It also opens up the possibility of composing slide decks by chaining
    them.
    
    This change also renames Deck to StaticDeck.

--- present.go
+++ present.go
@@ -5,44 +5,120 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-func Present(d *Deck) error {
-	ebiten.SetWindowSize(d.width, d.height)
+// Present launches a new window and starts the presentation.
+//
+// The launched presentation provides basic keyboard and mouse
+// controls for navigating the SlideDeck. The right arrow key
+// and left mouse button will advance the SlideDeck forward by
+// calling Next, while the left arrow key and right mouse button
+// will advance the SlideDeck backward by calling Prev.
+//
+// Present must be called from the program's main thread. Since
+// eep is imported, the main goroutine will be locked to the
+// main thread, so this must be called from the main goroutine.
+//
+// It does not return unless the window has exited or deck's
+// Update returns a non-nil error.
+func Present(width, height int, deck SlideDeck) error {
+	p := &presentation{width, height, deck}
+	ebiten.SetWindowSize(width, height)
 	ebiten.SetWindowTitle("Ebitengine Presentation")
-	return ebiten.RunGame(d)
+	return ebiten.RunGame(p)
 }
 
-type Deck struct {
+type presentation struct {
 	width, height int
-	currSlide     int
-	slides        []Slide
+	deck          SlideDeck
 }
 
-func NewDeck(width, height int) *Deck {
-	return &Deck{width: width, height: height}
+func (p *presentation) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return p.width, p.height
 }
 
-func (d *Deck) Append(s ...Slide) {
+func (p *presentation) Update() error {
+	switch {
+	case inpututil.IsKeyJustPressed(ebiten.KeyLeft) || inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonRight):
+		p.deck.Prev()
+	case inpututil.IsKeyJustPressed(ebiten.KeyRight) || inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft):
+		p.deck.Next()
+	}
+	return p.deck.Update()
+}
+
+func (p *presentation) Draw(screen *ebiten.Image) {
+	p.deck.Draw(screen)
+}
+
+// SlideDeck is a Slide that can advance forward or backward.
+type SlideDeck interface {
+	Slide
+
+	// Next advances the deck forward in response to mouse/keyboard input.
+	//
+	// Returns true on success. A call to Prev might fail to advance if,
+	// for example, there's no next slide (already on the last slide).
+	Next() DeckStatus
+
+	// Prev advances the deck backward in response to mouse/keyboard input.
+	//
+	// Returns true on success. A call to Prev might fail to advance if,
+	// for example, there's no previous slide (already on the first slide).
+	Prev() DeckStatus
+}
+
+// DeckStatus is the status of the SlideDeck after calling Next or Prev.
+type DeckStatus int
+
+const (
+	// DeckOK indicates that the Next or Prev call succeeded.
+	DeckOK DeckStatus = iota
+
+	// DeckBusy indicates that the Next or Prev call failed because
+	// the Deck is currently busy showing the viewer something.
+	// Typically, this is used to indicate that some animated
+	// transition is playing back.
+	DeckBusy
+
+	// DeckEnd indicates that the Next or Prev call failed because there
+	// are no more slides after or before respectively.
+	DeckEnd
+)
+
+// StaticDeck is a SlideDeck that contains a static set of Slides.
+type StaticDeck struct {
+	currSlide int
+	slides    []Slide
+}
+
+// Append adds a Slide to the deck.
+func (d *StaticDeck) Append(s ...Slide) {
 	d.slides = append(d.slides, s...)
 }
 
-func (d *Deck) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return d.width, d.height
+// Next advances the slide deck forward.
+func (d *StaticDeck) Next() DeckStatus {
+	if d.currSlide < len(d.slides)-1 {
+		d.currSlide++
+		return DeckOK
+	}
+	return DeckEnd
 }
 
-func (d *Deck) Update() error {
-	switch {
-	case inpututil.IsKeyJustPressed(ebiten.KeyLeft) || inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonRight):
-		if d.currSlide > 0 {
-			d.currSlide--
-		}
-	case inpututil.IsKeyJustPressed(ebiten.KeyRight) || inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft):
-		if d.currSlide < len(d.slides)-1 {
-			d.currSlide++
-		}
+// Prev advances the slide deck backward.
+func (d *StaticDeck) Prev() DeckStatus {
+	if d.currSlide > 0 {
+		d.currSlide--
+		return DeckOK
 	}
+	return DeckEnd
+}
+
+// Update implements Slide by updating the current Slide.
+func (d *StaticDeck) Update() error {
 	return d.slides[d.currSlide].Update()
 }
 
-func (d *Deck) Draw(screen *ebiten.Image) {
+// Draw implements Slide by drawing the current Slide.
+func (d *StaticDeck) Draw(screen *ebiten.Image) {
 	d.slides[d.currSlide].Draw(screen)
 }
--- slide.go
+++ slide.go
@@ -5,12 +5,49 @@ import (
 )
 
 // Slide represents a single slide in the slide deck.
+//
+// Slide is a subset of the ebiten.Game interface. This is intentional,
+// since it has the same split between update logic and draw logic.
+//
+// See [github.com/hajimehoshi/ebiten/v2.Game] for more context and
+// details.
+// See https://ebitencookbook.vercel.app/blog for an explanation of
+// the split between Update and Draw.
 type Slide interface {
+	// Draw renders the slide deck to the screen.
+	//
+	// Simple, static slides need only implement Draw; Update can be a no-op.
+	//
+	// Draw is called in lock-step with the refresh rate of the screen.
+	// It does not guarantee a fixed time delta between calls, and so
+	// is not ideal for any logic that causes your slide to evolve over
+	// time, especially if your logic includes any kind of physical
+	// simulation.
+	//
+	// Note that it's still feasible to do so if you wish to compute the
+	// time delta between frames, but this is likely much more complicated
+	// than you need for a slideshow presentation.
 	Draw(*ebiten.Image)
+
+	// Update is called once per tick.
+	//
+	// Update is where to put logic that influences what is drawn
+	// to the screen. For example, anything depending on time evolution,
+	// like a physical simulation.
+	//
+	// Ebiten will arrange for update to be called, in the long run,
+	// on average, 60 times per second. As a result, Update may
+	// assume that 1/60th of a second passes between each call.
+	//
+	// If Update returns a non-nil error, the presentation will exit.
+	// Update may return ebiten.Termination for a clean exit.
 	Update() error
 }
 
 // Static returns a slide that only has a Draw command.
+//
+// Without an Update function, the Slide is not expected
+// to evolve over time. Hence 'Static.'
 func Static(f func(screen *ebiten.Image)) Slide {
 	return staticSlide{f}
 }
@@ -27,6 +64,11 @@ func (s staticSlide) Update() error {
 	return nil
 }
 
+// Overlay returns a slide that draws dst, then src0 on top
+// of it, then src on top of that, etc.
+//
+// The returned slide will also execute all slides' Update
+// methods in its Update method.
 func Overlay(dst, src0 Slide, src ...Slide) Slide {
 	return slideStack{slides: append([]Slide{dst, src0}, src...)}
 }
